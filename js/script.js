bf47c15ef6a0a4b83d4dc7c04ef3e4fa3cd59a44
Velerium <bondaniandrea@libero.it>
2021-05-26 14:26:49 +0200

    Replaced two elements (in social and footer) with JS counterparts.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.getElementById('social-title').innerHTML='Seguici sui social media';
+
+var x = 10**3 * 2;
+var y = 10*3/2 + 3*2;
+
+var result = x + y;
+
+document.getElementById('year').innerHTML= result;
